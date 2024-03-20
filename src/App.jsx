cf7890e08b87da45db96495f1e053b7a96935203
simyikong <s2177224@siswa.um.edu.my>
2024-03-20 23:50:35 +0800

    /stock-data should navigate to AllStocks.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
               <Route path='*' element={<Overview />} />
               <Route path='/ai' element={<Ai />} />
               <Route path='/profile' element={<Profile />} />
-              <Route path='/stock-data' element={<StockDetails />} />
+              {/*<Route path='/stock-data' element={<StockDetails />} />*/}
               <Route path='/community' element={<Community />} />
               <Route path="/stock-data" element={<AllStocks />}/>
               <Route path='/editProfile' element={<EditProfile />} />
