aec894558bf77f183773b1a507bf1928bc10adf2
yeeern1217 <chengyeeern@yahoo.com>
2024-03-20 23:55:52 +0800

    improved groups

--- src/components/Community/Groups/Groups.jsx
+++ src/components/Community/Groups/Groups.jsx
@@ -1,37 +1,41 @@
 import React, { useState } from 'react';
-import { Posts } from '../dummyData.js';
-import '../Feed/Feed.css';
-import Post from '../Post/Post';
-import Share from '../Share/Share';
+import './Groups.css'; // 
+import { groups } from './../dummyData.js'; 
 
-export default function Groups() {
-  const [feedType, setFeedType] = useState('newest');
-
-  const handleFeedTypeChange = (type) => {
-    setFeedType(type);
-  };
-
-  const filteredPosts = feedType === 'newest' ? Posts : [];
+function Group() { 
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   return (
-    <div className="feed">
-      {/* Feed Controls */}
-      <div className="feedControls">
-        <button className={feedType === 'newest' ? 'active' : ''} onClick={() => handleFeedTypeChange('newest')}>
-          NewestGrp
-        </button>
-        <button className={feedType === 'trending' ? 'active' : ''} onClick={() => handleFeedTypeChange('trending')}>
-          TrendingGrp
-        </button>
-      </div>
-      
-      {/* Feed Wrapper */}
-      <div className="feedWrapper">
-        <Share />
-        {filteredPosts.map((p) => (
-          <Post key={p.id} post={p} />
+    <div className="groupContainer">
+      <div className="groupList">
+        {groups.map((group, index) => (
+          <div 
+            key={index} 
+            className="groupRow"
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+            style={{ backgroundColor: hoveredIndex === index ? '#107fac' : '' }}
+          >
+            <img className="groupCover" src={group.cover} alt={group.name} />
+            <div className="groupDetails">
+              <h2 className="groupName" style={{ color: hoveredIndex === index ? 'white' : 'black' }}>
+                {group.name}
+              </h2>
+              <p className="groupMemberCount" style={{ color: hoveredIndex === index ? 'white' : 'black' }}>
+                Members: {group.members}
+              </p>
+              <p className="groupCreated" style={{ color: hoveredIndex === index ? 'white' : 'black' }}>
+                Created: {group.created}
+              </p>
+              <p className="groupDescription" style={{ color: hoveredIndex === index ? 'white' : 'black' }}>
+                {group.description}
+              </p>
+            </div>
+          </div>
         ))}
       </div>
     </div>
   );
 }
+
+export default Group;
--- src/components/Community/dummyData.js
+++ src/components/Community/dummyData.js
@@ -286,3 +286,4 @@ export const Users = [
     // Add more groups here
   ];
   
+  
