6de7f6ce0aefd44f6368162a51fb88ddf71b83e3
yeeern1217 <chengyeeern@yahoo.com>
2024-03-24 18:56:02 +0800

    fix css

--- src/components/Learning/Module1/Module1KnowMore.jsx
+++ src/components/Learning/Module1/Module1KnowMore.jsx
@@ -9,9 +9,6 @@ const ModuleBox = ({ title, description, progress }) => {
     <div className={`module-box ${isCompleted ? 'completed' : ''}`}>
       <h2>{title}</h2>
       <p>{description}</p>
-      <div className="progress-bar">
-        <div className="progress" style={{ width: `${progress}%` }}></div>
-      </div>
       <div className="button-container">
         <NavLink to="/module1-chap1">
           <button className="start-button">View module</button>
@@ -29,22 +26,22 @@ const Module1KnowMore = () => (
     <ModuleBox
       title="Shariah Equities"
       description="Learn more about Shariah-compliant trading and investing"
-      progress={20}
+
     />
     <ModuleBox
       title="Shariah Derivatives"
       description="Learn more about Shariah-compliant derivatives products"
-      progress={100}
+
     />
     <ModuleBox
       title="Bursa Suq As-Sila’"
       description="Learn more about the commodity platform for Islamic Finance"
-      progress={40}
+
     />
     <ModuleBox
       title="Islamic Social Finance"
       description="Learn more about the socioeconomic importance in Islamic Finance"
-      progress={40}
+
     />
   </div>
 );
